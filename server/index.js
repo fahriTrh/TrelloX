685741ac021dff8365c75fe2e5250edebdd76fea
fahriTrh <fakhrialauddin44@gmail.com>
2025-10-04 21:28:38 +0700

    Re-added server as normal folder instead of nested repo

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { PrismaClient } = require('./generated/prisma')
+const { createServer } = require('node:http')
+const { Server } = require('socket.io')
+
+const app = express()
+app.use(express.json());
+
+
+// instansiasi
+const prisma = new PrismaClient();
+
+const server = createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CLIENT_URL,
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected', socket.id);
+})
+
+// api route
+app.route('/api')
+    // get fruit
+    .get(async (req, res) => {
+        const fruits = await prisma.fruit.findMany();
+
+        res.json(fruits);
+    })
+    // store fruit
+    .post(async (req, res) => {
+        const { fruit } = req.body;
+
+        const newFruit = await prisma.fruit.create({
+            data: { name: fruit }
+        })
+
+        res.json({ status: 200, message: 'data berhasil ditambahkan!', fruit: newFruit });
+        io.emit('new_fruit', newFruit)
+    })
+
+    // 
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+});
---
+++ server/prisma/migrations/20251004134128_task_manager/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Fruit" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Fruit_pkey" PRIMARY KEY ("id")
+);
